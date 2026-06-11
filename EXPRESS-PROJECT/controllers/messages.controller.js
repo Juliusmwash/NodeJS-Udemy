53d85f1a5966f9c56f1ae9b6d9baa8b2ffba1d29
Juliusmwash <cutejuliusmwash@gmail.com>
2026-06-12 02:02:35 +0300

    How to send file in express (in our case an image file)

--- EXPRESS-PROJECT/controllers/messages.controller.js
+++ EXPRESS-PROJECT/controllers/messages.controller.js
@@ -1,13 +1,16 @@
+const path = require('path');
+
 function getMessages(req, res) {
-	res.send('<ul><li>Helloo Albert!</li></ul>');
+	res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpeg'));
+	// res.send('<ul><li>Helloo Albert!</li></ul>');
 }
 
 function postMessage(req, res) {
 	console.log('Updating messages...');
-    res.json({success: "message updated successfully"});
+	res.json({ success: "message updated successfully" });
 }
 
 module.exports = {
-    getMessages,
-    postMessage
+	getMessages,
+	postMessage
 };
