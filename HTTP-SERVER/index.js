7c6cf195bf491b47e6eaed3d8f87f33dc517ce75
Juliusmwash <cutejuliusmwash@gmail.com>
2026-06-11 12:19:45 +0300

    Using parameterized urls

--- HTTP-SERVER/index.js
+++ HTTP-SERVER/index.js
@@ -3,44 +3,50 @@ const http = require('http');
 const PORT = 3000;
 
 const friends = [
-    {
-        id: 0,
-        name: 'Nikola Tesla'
-    },
-    {
-        id: 1,
-        name: 'Sir Isaac Newton'
-    },
-    {
-        id: 2,
-        name: 'Albert Einstein'
-    }
+	{
+		id: 0,
+		name: 'Nikola Tesla'
+	},
+	{
+		id: 1,
+		name: 'Sir Isaac Newton'
+	},
+	{
+		id: 2,
+		name: 'Albert Einstein'
+	}
 ]
 
 const server = http.createServer();
 server.listen(PORT);
 
 server.on('request', (req, res) => {
-    const items = req.url.split('/')
-    if (req.url === '/friends') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(friends));
-    } else if (req.url === '/messages') {
+	const items = req.url.split('/') // /friends/2 => ['', 'friends', '2']
+	if (items[1] === 'friends') {
 		res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.write('<html>');
-        res.write('<body>');
-        res.write('<ul>');
-        res.write('<li>Hello Isaac!</li>');
-        res.write('<li>What are your thoughts on astronomy?</li>');
-        res.write('</html>');
-        res.write('</body>');
-        res.write('</ul>');
-        res.end();
-    } else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ info: 'Sorry, not implemented yet' }));
-    }
+		res.setHeader('Content-Type', 'application/json');
+
+		if (items.length === 3) {
+			const friendIndex = parseInt(items[2]);
+			res.end(JSON.stringify(friends[friendIndex]))
+		} else {
+			res.end(JSON.stringify(friends));
+		}
+	} else if (items[1] === '/messages') {
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.write('<html>');
+		res.write('<body>');
+		res.write('<ul>');
+		res.write('<li>Hello Isaac!</li>');
+		res.write('<li>What are your thoughts on astronomy?</li>');
+		res.write('</html>');
+		res.write('</body>');
+		res.write('</ul>');
+		res.end();
+	} else {
+		res.statusCode = 404;
+		res.setHeader('Content-Type', 'application/json');
+		res.end(JSON.stringify({ info: 'Sorry, not implemented yet' }));
+	}
 })
