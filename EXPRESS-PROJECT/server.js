b57d212a18c9c212191084c3bc77b2c008828a98
Juliusmwash <cutejuliusmwash@gmail.com>
2026-06-11 23:30:26 +0300

    improve one of the Middleware and implement a logic to receive post data

--- EXPRESS-PROJECT/server.js
+++ EXPRESS-PROJECT/server.js
@@ -14,17 +14,53 @@ const friends = [
 		name: 'Sir Isaac Newton'
 	},
 	{
-		id: 1,
+		id: 2,
 		name: 'Julius Mwangi'
 	}
 ]
 
+/*
 app.use((req, res, next) => {
 	const start = Date.now();
 	next();
 	const delta = Date.now() - start;
 	console.log(`${req.method} ${req.url} ${delta}ms`);
 })
+*/
+
+// The following is more accurate incase asynchronous work is started in between the process
+app.use((req, res, next) => {
+	const start = Date.now();
+
+	res.on('finish', () => {
+		console.log(
+			`${req.method} ${req.url} ${Date.now() - start}ms`
+		);
+	});
+
+	next();
+});
+
+app.use(express.json());
+
+app.post('/friends', (req, res) => {
+	console.log(req.headers['content-type']);
+	console.log(req.body);
+
+	//if (!req.body.name)
+	if (!req.body?.name) {  // The ?. operator safely handles req.body === undefined, incase the Content-Type header is not set to application/json
+		return res.status(400).json({
+			error: 'Missing friend name'
+		})
+	}
+	const newFriend = {
+		id: friends.length,
+		name: req.body.name
+	};
+	friends.push(newFriend);
+
+	res.json(newFriend);
+});
 
 app.get('/', (req, res) => {
 	res.send('Heeeellooooooo!');
