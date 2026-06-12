e696590ba00e490975de540f89cf8cc1da62a588
Juliusmwash <cutejuliusmwash@gmail.com>
2026-06-12 21:30:38 +0300

    Add views for the Handlebars template engine used in our app

--- EXPRESS-PROJECT/controllers/messages.controller.js
+++ EXPRESS-PROJECT/controllers/messages.controller.js
@@ -1,7 +1,11 @@
 const path = require('path');
 
 function getMessages(req, res) {
-	res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpeg'));
+	res.render('messages', {
+		title: "Messages to my friends!",
+		friend:  "Elon Musk"
+	});
+	//res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpeg'));
 	// res.send('<ul><li>Helloo Albert!</li></ul>');
 }
 
