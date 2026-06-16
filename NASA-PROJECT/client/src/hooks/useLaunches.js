12f97844416109e3f5c1f87ab6f003ffc222d7ae
Juliusmwash <cutejuliusmwash@gmail.com>
2026-06-16 23:12:38 +0300

    Connecting POST /launches with front end dashboard

--- NASA-PROJECT/client/src/hooks/useLaunches.js
+++ NASA-PROJECT/client/src/hooks/useLaunches.js
@@ -21,7 +21,7 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
 
   const submitLaunch = useCallback(async (e) => {
     e.preventDefault();
-    // setPendingLaunch(true);
+    setPendingLaunch(true);
     const data = new FormData(e.target);
     const launchDate = new Date(data.get("launch-day"));
     const mission = data.get("mission-name");
@@ -34,14 +34,13 @@ function useLaunches(onSuccessSound, onAbortSound, onFailureSound) {
       target,
     });
 
-    // TODO: Set success based on response.
-    const success = false;
+    const success = response.ok;
     if (success) {
       getLaunches();
       setTimeout(() => {
         setPendingLaunch(false);
         onSuccessSound();
-      }, 800);
+      }, 8000);
     } else {
       onFailureSound();
     }
--- NASA-PROJECT/server/src/models/launches.model.js
+++ NASA-PROJECT/server/src/models/launches.model.js
@@ -6,8 +6,8 @@ const launch = {
     flightNumber: 100,
     mission: 'Kepler Exploration X',
     rocket: 'Explorer IS1',
-    launchDate: new Date('December 27, 2030'),
-    destination: 'Kepler-442 b',
+    launchDate: new Date('December 29, 2030'),
+    target: 'Kepler-442 b',
     customer: ['ZTM', 'NASA'],
     upcoming: true,
     success: true,
@@ -24,9 +24,9 @@ function addNewLaunch(launch) {
     launches.set(
         latestFlightNumber,
         Object.assign(launch, {
-            success: true,
-            upcoming: true,
             customer: ['Zero to Mastery', 'NASA'],
+            upcoming: true,
+            success: true,
             flightNumber: latestFlightNumber,
         })
     );
