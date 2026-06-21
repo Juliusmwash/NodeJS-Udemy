b49299f409ff9733cd92ae7d378986e74b60ace0
Juliusmwash <cutejuliusmwash@gmail.com>
2026-06-21 21:52:06 +0300

    Building a simple blocking server

---
+++ PERFORMANCE-EXAMPLE/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const app = express();
+
+function delay(duration) {
+    const startTime = Date.now();
+    while(Date.now() - startTime < duration) {
+        // Event loop is blocked...
+    }
+}
+
+app.get('/home', (req, res) => {
+    res.send('Performance example');
+});
+
+app.get('/timer', (req, res) => {
+    // Delay the response
+    delay(9000);
+    res.send('Ding ding ding!');
+});
+
+app.listen(3000);
