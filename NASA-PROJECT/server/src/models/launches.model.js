a0088a64a3ed9fcc51e57ee8f8caf9a258bc5c52
Juliusmwash <cutejuliusmwash@gmail.com>
2026-07-09 03:53:29 +0300

    Connect spaceX API into our Nasa Project

--- NASA-PROJECT/server/src/models/launches.model.js
+++ NASA-PROJECT/server/src/models/launches.model.js
@@ -1,12 +1,9 @@
+const axios = require('axios');
+
 const launches = require('./launches.mongo');
 const planets = require('./planets.mongo')
 
 const DEFAULT_FLIGHT_NUMBER = 100;
-
-async function existsLaunchWithId(launchId) {
-	return await launches
-		.findOne({ 'flightNumber': launchId });
-}
 
 async function getLatestFlightNumber() {
 	const latestLaunch = await launches
@@ -27,17 +24,6 @@ async function getAllLaunches() {
 }
 
 async function saveLaunch(launch) {
-	const planet = await planets.findOne({
-		keplerName: launch.target
-	});
-
-	if (!planet) {
-		const error = new Error('No matching planet found!');
-		error.code = 'PLANET_NOT_FOUND';
-		
-		throw error;
-	}
-
 	await launches.findOneAndUpdate({
 		flightNumber: launch.flightNumber,
 	}, launch, {
@@ -45,7 +31,95 @@ async function saveLaunch(launch) {
 	});
 }
 
+async function populateLaunches() {
+	console.log('Downloading launch data...');
+
+	let THESPACEDEVS_API_URL = 'https://lldev.thespacedevs.com/2.4.0/launches/?limit=100';
+	let launchObjectsArray = [];
+
+	while (THESPACEDEVS_API_URL) {
+		const response = await axios.get(THESPACEDEVS_API_URL);
+		const upcomingStatuses = [
+			"Go",
+			"Hold",
+			"TBD",
+			"TBC",
+			"In Flight"
+		];
+
+		if (response.status !== 200) {
+			console.log('Problem downloading launch data.');
+			throw new Error('Launch data download failed.');
+		}
+
+		launchObjectsArray.push(
+			...response.data.results.map((launchResult) => {
+				const flightNumber = parseInt(launchResult.mission?.id, 10);
+
+				if (!Number.isFinite(flightNumber)) {
+					console.log('Failed to parse launchResult.mission?.id into a Number\n');
+					return null;
+				}
+				return {
+					flightNumber,
+					mission: launchResult.mission?.name ?? launchResult.name,
+					rocket: launchResult.rocket?.configuration?.name,
+					launchDate: new Date(launchResult.net),
+					customers: launchResult.mission?.agencies?.map(a => a.name) ?? [],
+					upcoming: upcomingStatuses.includes(launchResult.status?.abbrev),
+					success: launchResult.status?.abbrev === "Success",
+				};
+			})
+		);
+
+		THESPACEDEVS_API_URL = response.data.next;
+	}
+
+	console.log('Saving data to the database...');
+	await Promise.all(
+		launchObjectsArray
+			.filter(launch => launch !== null)
+			.map(launch => saveLaunch(launch))
+	);
+
+	console.log(`Done saving ${launchObjectsArray.length} launches`);
+	// console.log(launchObjectsArray);
+}
+
+async function loadLaunchData() {
+	const firstLaunch = await findLaunch({
+		flightNumber: 6302,
+		rocket: 'H3-24',
+		mission: 'HTV-X3',
+	});
+
+	if (firstLaunch) {
+		console.log('Launch data already loaded');
+	} else {
+		await populateLaunches();
+	}
+}
+
+async function findLaunch(filter) {
+	return await launches.findOne(filter);
+}
+
+async function existsLaunchWithId(launchId) {
+	return await findLaunch({ 'flightNumber': launchId });
+}
+
 async function addNewLaunch(launch) {
+	const planet = await planets.findOne({
+		keplerName: launch.target
+	});
+
+	if (!planet) {
+		const error = new Error('No matching planet found!');
+		error.code = 'PLANET_NOT_FOUND';
+
+		throw error;
+	}
+
 	const newFlightNumber = await getLatestFlightNumber() + 1;
 
 	const newLaunch = Object.assign(launch, {
@@ -77,6 +151,7 @@ async function abortLaunchById(launchId) {
 module.exports = {
 	existsLaunchWithId,
 	getAllLaunches,
+	loadLaunchData,
 	addNewLaunch,
 	abortLaunchById
 };
