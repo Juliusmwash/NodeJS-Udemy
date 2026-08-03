522a60cb8aa5364bdc0f18fef2202ec47898ce8f
Juliusmwash <cutejuliusmwash@gmail.com>
2026-08-03 03:27:36 +0300

    Using Resolvers

--- GRAPHQL-EXAMPLE/server.js
+++ GRAPHQL-EXAMPLE/server.js
@@ -6,12 +6,26 @@ const { loadFilesSync } = require('@graphql-tools/load-files');
 const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 const typesArray = loadFilesSync('**/*', {
-  extensions: ['graphql'],
+	extensions: ['graphql'],
 });
 
 const schema = makeExecutableSchema({
-	typeDefs: typesArray
-})
+	typeDefs: typesArray,
+	resolvers: {
+		Query: {
+			products: async (parent, args, context, info) => {
+				console.log('Getting the products...');
+				const products = await Promise.resolve(parent.products);
+				return products;
+			},
+			orders: async (parent, args, context, info) => {
+				console.log('Getting the orders...');
+				const orders = await Promise.resolve(parent.orders);
+				return orders;
+			},
+		}
+	}
+});
 
 const root = {
 	products: require('./products/products.model.js'),
